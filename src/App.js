5254b75f7076304c52d838f2ab379a72c4f7060e
abolfazl_mousavi_amjad <doctorweb2023@gmail.com>
2024-10-21 00:14:10 +0330

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaTimesCircle } from "react-icons/fa";
+import { VscThreeBars } from "react-icons/vsc";
+import Links from "./data";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+
+  const [showLinks, setShowLinks] = useState(false);
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "";
+    }
+  }, [showLinks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="right-navbar">
+          <h3>وبسایت شخصی من</h3>
+          <button onClick={() => setShowLinks(!showLinks)}>
+            {showLinks ? <FaTimesCircle /> : <VscThreeBars />}
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul ref={linksRef}>
+            {
+              Links.map((link, index) => (
+                <li key={index}><a href={link.url}>{link.title}</a></li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const Links = [
+    {
+       id: 1,
+       title: "صفحه ی اصلی",
+       url: "/" 
+    },
+    {
+        id: 2,
+        title: "فروشگاه",
+        url: "/"
+    },
+    {
+        id: 3,
+        title: "وبلاگ",
+        url: "/"
+    },
+    {
+        id: 4,
+        title: "تماس با ما",
+        url: "/"
+    },
+    {
+        id: 5,
+        title: "درباره ما",
+        url: "/"
+    }
+];
+
+export default Links;
